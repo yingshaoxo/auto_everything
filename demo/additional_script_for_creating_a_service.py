cf4f03c0e2f327a7cedda836f788ce651cd73ff4
yingshaoxo <yingshaoxo@gmail.com>
2018-05-26 12:04:16 +0800

    update

--- demo/additional_script_for_creating_a_service.py
+++ demo/additional_script_for_creating_a_service.py
@@ -1,18 +1,13 @@
-from auto_everything.base import Terminal, IO
-import time
+from auto_everything.base import Terminal, IO, Python
 
 t = Terminal()
 io = IO()
+py = Python()
 
-while 1:
-    try:
-        if not t.is_running("firefox"):
-            #io.log("no running")
-            t.run_program("firefox")
+@py.loop()
+def do():
+    if not t.is_running("firefox"):
+        #io.log("no running")
+        t.run_program("firefox")
 
-        if not t.is_running("terminator"):
-            t.run_program("terminator")
-
-        time.sleep(1)
-    except Exception as e:
-        log(e)
+do()
