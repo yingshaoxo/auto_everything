cff71c0ecd81110079b9a4403e0baee65dd48d31
yingshaoxo <yingshaoxo@gmail.com>
2018-11-19 18:14:38 +0800

    no need to change this again

--- demo/video/hi.py
+++ demo/video/hi.py
@@ -1,7 +1,7 @@
 from auto_everything.video import Video
 
 video = Video("/home/yingshaoxo/Videos/demo.mp4")
-video.humanly_remove_silence_parts_from_video(db_for_split_silence_and_voice=20)
+video.humanly_remove_silence_parts_from_video(db_for_split_silence_and_voice=12)
 
 #video = Video()
 #video_file_path = video.combine_all_mp4_in_a_folder("/home/yingshaoxo/Videos/doing")
