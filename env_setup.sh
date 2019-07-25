84b8bd139ba099eb2b67218582984657d844b7c7
yingshaoxo <yingshaoxo@gmail.com>
2019-07-25 15:16:32 +0800

    update env_setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,16 +1,13 @@
 sudo apt -y install python3
 sudo apt -y install python3-pip
-
-sudo yum -y update
-sudo yum -y install yum-utils
-sudo yum -y groupinstall development
-sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-sudo yum -y install python36u
-sudo cp /usr/bin/python3.6 /usr/bin/python3
-sudo yum -y install python36u-pip
-sudo cp /usr/bin/pip3.6 /usr/bin/pip3
-sudo yum -y install python36u-devel
-
 sudo pip3 install auto_everything --upgrade
+
+sudo pacman --noconfirm -Syu python-tensorflow-cuda
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip3 install psuti
+sudo pip3 install fire
+sudo pacman --noconfirm -S cmake
+sudo pacman --noconfirm -S make
 
 #sudo apt install ffmpeg
