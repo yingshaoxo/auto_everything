1a53dd3c41103c3ff4e83858fd3f7a45e06c32c4
yingshaoxo <yingshaoxo@users.noreply.github.com>
2019-11-16 23:38:19 +0800

    add gcc

--- env_setup.sh
+++ env_setup.sh
@@ -1,11 +1,13 @@
 sudo apt -y install python3
 sudo apt -y install python3-pip
+sudo apt -y install gcc
 
 #sudo pacman --noconfirm -Syu python-tensorflow-cuda
 sudo pacman --noconfirm -Syu python3
 curl -O https://bootstrap.pypa.io/get-pip.py
 sudo python get-pip.py
 
+sudo pacman --noconfirm -S gcc
 sudo pacman --noconfirm -S cmake
 sudo pacman --noconfirm -S make
 
