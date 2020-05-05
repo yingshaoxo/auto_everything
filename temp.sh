73f0be887504492d7a981080bb0f32fbe8d8e468
yingshaoxo <yingshaoxo@gmail.com>
2020-05-06 01:12:07 +0800

    video: add threads to speed up the process of video;better info printing; change the logic of humanly...; add extra_require

--- temp.sh
+++
@@ -1,8 +0,0 @@
-
-        sudo apt install python3-sphinx
-        sudo pip3 install Flask-Sphinx-Themes
-        cd docs
-        make html
-        cp docs/html/* . -fr
-        rm docs -fr
-        
