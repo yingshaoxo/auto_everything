547d6d8ad95a966bfbdcced9f8fa22ebfc16bcd5
yingshaoxo <yingshaoxo@gmail.com>
2020-06-19 02:21:46 +0800

    web: fix the timeout problem

--- auto_everything/web.py
+++ auto_everything/web.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 
 
 class Selenium():
@@ -10,7 +11,7 @@ class Selenium():
     It's a wrap of selenium
     """
 
-    def __init__(self, url, headless=False):
+    def __init__(self, url, headless=False, timeout=500):
         try:
             if headless == False:
                 self.driver = webdriver.Chrome()
@@ -26,9 +27,13 @@ class Selenium():
             except Exception as e:
                 print(e)
                 self.driver = webdriver.PhantomJS()
+
         self.driver.get(url)
 
-    def wait_until_exists(self, xpath, timeout=600):
+        self.driver.set_page_load_timeout(timeout)
+        # self.driver.implicitly_wait(timeout)
+
+    def wait_until_exists(self, xpath, timeout=20):
         """
         wait elements to be exist in an webpage, return those elements
 
@@ -43,6 +48,10 @@ class Selenium():
         try:
             w = WebDriverWait(self.driver, timeout)
             w.until(presence_of_element_located((By.XPATH, xpath)))
+            elements = self.driver.find_elements_by_xpath(xpath)
+            return elements
+        except TimeoutException as e:
+            print(e)
             elements = self.driver.find_elements_by_xpath(xpath)
             return elements
         except Exception as e:
