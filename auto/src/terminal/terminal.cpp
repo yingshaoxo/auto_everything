9d862feadc00ee9ff35f9acb0540fdf478b30a2b
yingshaoxo <yingshaoxo@gmail.com>
2021-02-14 23:16:34 +0800

    add cpp version of run and run_commands to termianl module

--- auto/src/terminal/terminal.cpp
+++ auto/src/terminal/terminal.cpp
@@ -1,12 +1,36 @@
 #include <pybind11/pybind11.h>
 #include <iostream>
+#include <string>
+#include <bits/stdc++.h> 
+
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int run(int i) {
-    std::cout << i <<std::endl;
-    return i;
+void run(std::string commands) {
+    auto c_string = commands.c_str(); 
+    std::system(c_string); 
+    //std::cout << "result" <<std::endl;
+}
+
+
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <array>
+std::string run_commands(std::string commands) {
+    const char* cmd = commands.c_str();
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
 }
 
 namespace py = pybind11;
@@ -22,7 +46,12 @@ PYBIND11_MODULE(terminal, m) {
     )pbdoc";
 
     m.def("run", &run, R"pbdoc(
-        Add two numbers
+        run some commands
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("run_commands", &run_commands, R"pbdoc(
+        run some commands and return results
         Some other explanation about the add function.
     )pbdoc");
 
