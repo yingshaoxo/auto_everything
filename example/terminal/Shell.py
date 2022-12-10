bc0f57e4ddedaf76899ed030a51638dc01c365f2
yingshaoxo <yingshaoxo@gmail.com>
2022-12-10 16:18:12 +0800

    terminal: upgrade the shell to support history command search; bapass the arrow keys

--- example/terminal/Shell.py
+++ example/terminal/Shell.py
@@ -2,36 +2,44 @@
 import os, re
 import tty, termios, sys
 
+from typing import Any
+
 from auto_everything.python import Python
 from auto_everything.terminal import Terminal
+from auto_everything.disk import Store
 
 py = Python()
 t = Terminal(debug=False)
 
+store = Store("auto_everything_shell")
+history_command_list_store_key = "history_command_list"
+final_search_result_store_key = "final_search_result"
+
 MODE = None
 
-"""
-def my_shell(type=None):
-    if type == "x":
-        def command_line_transforming(command):
-            return "proxychains4 " + command
-    else:
-        def command_line_transforming(command):
-            return command
+def search_a_command(text: str) -> list[str]:
+    keyword_list = text.split(" ")
+    history_command_list: list[str] = store.get(history_command_list_store_key, [])
 
-    t.debug = False
-    print("Welcome!\n\nLet's begin the journey by type your command here:\n")
-    print("> ", end="")
-    while True:
-        try:
-            command = input("")
-            t.run(command_line_transforming(command=command))
-            print()
-            print("> ", end="")
-        except Exception as e:
-            print(e)
-"""
+    match_commands = []
+    for command in history_command_list:
+        ok = True
+        for keyword in keyword_list:
+            if keyword not in command:
+                ok = False
+                break
+        if ok:
+            match_commands.append(command)
 
+    return match_commands
+
+def add_a_command_to_history(command: str):
+    command = command.strip()
+    history_command_list: list[str] = store.get(history_command_list_store_key, [])
+    if command not in history_command_list:
+        history_command_list.append(command)
+        store.set(history_command_list_store_key, history_command_list)
+        
 def get_char():
     #https://www.physics.udel.edu/~watson/scen103/ascii.html
 
@@ -61,8 +69,48 @@ def print_without_new_line(text):
     out.flush()
     """
 
-def print_with_new_line(text=""):
+def print_with_new_line(text:Any=""):
     print(text, end="\n", flush=True)
+
+def monitor_the_terminal_keyboard(callback_function_that_takes_input_string, exit_ascii_code:list[int]=[10, 12]):
+    command = ""
+    while True:
+        try:
+            char = get_char()
+            char_id = ord(char)
+
+            if char_id in [27, 91]: #unknown
+                continue
+            if char_id in [68, 65, 67, 66]: #arrow left,up,right,down
+                continue
+            if char_id in exit_ascii_code:
+                break
+
+            # Print each char on the screen
+            print_without_new_line(char)
+            if char.isprintable():
+                command += char
+
+            if char_id in [3, 4]:
+                # Exit on ctrl-c, ctrl-d
+                break
+            if char_id == 12:
+                # Clear screen when press ctrl-l
+                break
+            if char_id == 16:
+                # Ctrl+p
+                break
+            if char_id == 127:
+                # Delete key
+                delete_one_char()
+                command = command.strip("\n ")
+                command = command[:-1]
+            if char_id == 10 or char_id == 13:
+                break
+
+            callback_function_that_takes_input_string(command)
+        except Exception as e:
+            print(e)
 
 def command_process(command):
     global MODE
@@ -79,6 +127,7 @@ def command_process(command):
         command = f"proxychains4 {command}"
 
     t.run(command)
+    add_a_command_to_history(command=command)
 
 def my_shell():
     # how to measure if a shell program is working properly? Can use it to use vim.
@@ -93,14 +142,22 @@ def my_shell():
             char = get_char()
             char_id = ord(char)
 
+            # print_with_new_line(char_id)
+            # if char_id in [3, 4]:
+            #     # Exit on ctrl-c, ctrl-d
+            #     break
+            # continue
+
+            if char_id in [27, 91]: #unknown
+                continue
+            if char_id in [68, 65, 67, 66]: #arrow left,up,right,down
+                continue
+
             # Print each char on the screen
             print_without_new_line(char)
             if char.isprintable():
                 command += char
 
-            # if char_id in [3, 4, 26, 27]:
-            #     # Exit on ctrl-c, ctrl-d , ctrl-z, or ESC
-            #     break
             if char_id in [3, 4]:
                 # Exit on ctrl-c, ctrl-d
                 break
@@ -108,7 +165,32 @@ def my_shell():
                 # Clear screen when press ctrl-l
                 t.run("clear")
                 print_without_new_line("> ")
+            if char_id == 16:
+                # Ctrl+P to make a search for history commands
+                result = ""
+                def search_it(the_command):
+                    t.run("clear")
+
+                    result = "\n".join(search_a_command(the_command))
+                    store.set(final_search_result_store_key, result)
+                    print_without_new_line(result)
+
+                    print_without_new_line("\n\n" + "__________" + "\n\n")
+
+                    print_without_new_line("> Search: " + the_command )
+
+                t.run("clear")
+                print_without_new_line("> Search: ")
+                monitor_the_terminal_keyboard(
+                    search_it
+                )
+
+                t.run("clear")
+                print_with_new_line(store.get(final_search_result_store_key, ""))
+                print_with_new_line()
+                print_without_new_line("> ")
             if char_id == 127:
+                # Delete key
                 delete_one_char()
                 command = command.strip("\n ")
                 command = command[:-1]
