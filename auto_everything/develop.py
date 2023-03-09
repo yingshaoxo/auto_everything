2159d96ff9a5c9a0624d6ff3ea55616c4c32e184
yingshaoxo <yingshaoxo@gmail.com>
2023-03-09 13:21:56 +0800

    develop: add yrpc main logic, now it could be used to generate advanced data object without problems

--- auto_everything/develop.py
+++ auto_everything/develop.py
@@ -1,5 +1,7 @@
 import re
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
+
+from numpy import double
 
 from auto_everything.terminal import Terminal
 from auto_everything.io import IO
@@ -431,11 +433,348 @@ package {filename}_grpc_key_string_maps
 
 
 class YRPC:
-    pass
+    _yrpc_type_to_python_type_dict = {
+        "string": "str",
+        "bool": "bool",
+        "uint32": "int",
+        "int32": "int",
+        "int64": "int",
+        "uint64": "int",
+        "bytes": "str",
+        "float": "float",
+        "double": "float"
+    }
+
+    # def _get_python_type_from_yrpc_type(self, type_string: str):
+    #     if type_string in self._yrpc_type_to_python_type_dict:
+    #         return self._yrpc_type_to_python_type_dict.get(type_string)
+    #     else:
+    #         return None
+
+    def get_information_from_yrpc_protocol_code(self, source_code: str) -> Tuple[dict[str, Any], dict[str, Any]]:
+        code_block_list = re.findall(r"(?P<type>\w+)\s+(?P<object_name>\w+)\s+\{(?P<properties>(\s*.*?\s*)+)\}", source_code, re.DOTALL)
+        code_block_list = [
+                    [string for string in one][:3]
+                    for one in code_block_list
+                ]
+
+        new_parsed_object_list: list[Any] = []
+        for one in code_block_list.copy():
+            if len(one) == 3:
+                type_ = one[0]
+                class_name = one[1]
+                content = one[2]
+
+                if (type_ == "service"):
+                    new_parsed_object_list.append([type_.strip(), class_name.strip(), content])
+                else:
+                    property_text = " " + content.strip()
+                    if len(property_text) == 0:
+                        continue
+
+                    property_list = []
+                    if (type_ == "message"):
+                        property_list = re.findall(r"(?P<feature>\w*)\s+(?P<type>\w+)\s+(?P<property>\w+)\s+=\s+\d+;", property_text)
+                    elif (type_ == "enum"):
+                        property_list = re.findall(r"(?P<property>\w+)\s+=\s+\d+;", property_text)
+                        property_list = [('','string', one) for one in property_list]
+
+                    new_parsed_object_list.append([type_.strip(), class_name.strip(), property_list])
+        
+        class_name_list = [one[1].strip() for one in new_parsed_object_list]
+        for one in class_name_list:
+            if class_name_list.count(one) > 1:
+                raise Exception("You must make sure there has no duplicated class/message name.")
+
+        arguments_defination_tree:dict[str, Any] = {}
+        for one in new_parsed_object_list.copy():
+            code_block_type = one[0].strip()
+            class_name = one[1].strip()
+            variable_list = one[2]
+
+            if code_block_type == "service":
+                continue
+
+            name_list = [one[2] for one in variable_list]
+            for one in name_list:
+                if name_list.count(one) > 1:
+                    raise Exception("You must make sure there has no duplicated variable name.")
+
+            arguments_defination_tree[class_name] = {
+                "**type**": code_block_type
+            }
+            for one_variable in variable_list:
+                feature = one_variable[0]
+                type = one_variable[1]
+                name = one_variable[2]
+
+                arguments_defination_tree[class_name][name] = {
+                    "type": type, 
+                    "is_list": True if feature == "repeated" else False,
+                    "name": name,
+                    "feature": feature
+                }
+
+        rpc_defination_tree: dict[str, Any] = {}
+        for one in new_parsed_object_list.copy():
+            code_block_type = one[0].strip()
+            class_name = one[1].strip()
+            content = one[2]
+
+            if code_block_type != "service":
+                continue
+
+            variable_list: list[Any] = re.findall(r"rpc\s+(?P<function_name>\w+)\s+\((?P<input_variable>[\w\s]+)\)\s+returns\s+\((?P<output_variable>[\w\s]+)\);", content)
+
+            name_list = [one[0].strip() for one in variable_list]
+            for one in name_list:
+                if name_list.count(one) > 1:
+                    raise Exception("You must make sure there has no duplicated variable name.")
+
+            for one in variable_list:
+                function_name = one[0].strip()
+                input_variable = one[1].strip()
+                output_variable = one[2].strip()
+
+                rpc_defination_tree[function_name] = {
+                    "input_variable": input_variable,
+                    "output_variable": output_variable
+                }
+
+        return arguments_defination_tree, rpc_defination_tree
+
+    def _convert_yrpc_code_into_python_code(self, source_code: str) -> str:
+        arguments_dict, rpc_dict = self.get_information_from_yrpc_protocol_code(source_code=source_code)
+
+        enum_code_block_list: list[str] = []
+        dataclass_code_block_list: list[str] = []
+        for class_name, class_info in arguments_dict.items():
+            code_block_type = class_info["**type**"]
+            del class_info["**type**"]
+
+            if code_block_type == "enum":
+                variable_list: list[str] = []
+                for index, one in enumerate(class_info.values()):
+                    name = one['name']
+                    variable_list.append(f"""
+    {name} = {index}
+                    """.rstrip().lstrip('\n'))
+                variable_list_text = "\n".join(variable_list)
+
+                enum_class_text = f"""
+class {class_name}(Enum):
+{variable_list_text}
+                """.rstrip().lstrip('\n')
+
+                enum_code_block_list.append(enum_class_text)
+            else:
+                variable_list: list[str] = []
+                property_name_to_its_type_dict_variable_list: list[str] = []
+                key_string_dict_list: list[str] = []
+                for index, one in enumerate(class_info.values()):
+                    name = one['name']
+                    type = self._yrpc_type_to_python_type_dict.get(one['type']) 
+                    if type == None:
+                        if one['type'] in arguments_dict.keys():
+                            type = one['type']
+                        else:
+                            raise Exception(f"We don't support type of '{one['type']}'")
+                    is_list = one['is_list']
+
+                    if is_list == False:
+                        variable_list.append(f"""
+    {name}: {type} | None = None
+                        """.rstrip().lstrip('\n'))
+                    else:
+                        variable_list.append(f"""
+    {name}: list[{type}] | None = None
+                        """.rstrip().lstrip('\n'))
+
+                    property_name_to_its_type_dict_variable_list.append(f"""
+        "{name}": {type},
+                    """.rstrip().lstrip('\n'))
+
+                    key_string_dict_list.append(f"""
+        {name}: str = "{name}"
+                    """.rstrip().lstrip('\n'))
+
+                variable_list_text = "\n".join(variable_list)
+                property_name_to_its_type_dict_variable_list_text = "\n".join(property_name_to_its_type_dict_variable_list)
+                key_string_dict_list_text = "\n".join(key_string_dict_list)
+                if len(key_string_dict_list_text.strip()) == 0:
+                    key_string_dict_list_text = """
+        pass
+                    """.rstrip().lstrip('\n')
+
+                dataclass_text = f"""
+@dataclass()
+class {class_name}(YRPC_OBJECT_BASE_CLASS):
+{variable_list_text}
+
+    property_name_to_its_type_dict_ = {{
+{property_name_to_its_type_dict_variable_list_text}
+    }}
+
+    @dataclass()
+    class key_string_dict_:
+{key_string_dict_list_text}
+                """.rstrip().lstrip('\n')
+
+                dataclass_code_block_list.append(dataclass_text)
+
+        enum_code_block_list_text = "\n\n\n".join(enum_code_block_list)
+        dataclass_code_block_list_text = "\n\n\n".join(dataclass_code_block_list)
+
+        template_text = f"""
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any
+
+
+_ygrpc_official_types = [int, float, str, bool]
+
+
+{enum_code_block_list_text}
+
+
+def convert_dict_that_has_enum_object_into_pure_dict(value: Any) -> dict[str, Any] | list[Any] | Any:
+    if type(value) is list:
+        new_list: list[Any] = []
+        for one in value: #type: ignore
+            new_list.append(convert_dict_that_has_enum_object_into_pure_dict(value=one)) 
+        return new_list
+    elif type(value) is dict:
+        new_dict: dict[str, Any] = {{}}
+        for key_, value_ in value.items(): #type: ignore
+            new_dict[key_] = convert_dict_that_has_enum_object_into_pure_dict(value=value_) #type: ignore
+        return new_dict
+    else:
+        if str(type(value)).startswith("<enum"):
+            return value.name
+        else:
+            if type(value) in _ygrpc_official_types:
+                return value
+            else:
+                # handle custom message data type
+                if value == None:
+                    return None
+                else:
+                    return convert_dict_that_has_enum_object_into_pure_dict(
+                        value=value.to_dict()
+                    )
+
+
+def convert_pure_dict_into_a_dict_that_has_enum_object(pure_value: Any, refrence_value: Any) -> Any:
+    if type(pure_value) is list:
+        new_list: list[Any] = []
+        for one in pure_value: #type: ignore
+            new_list.append(
+                convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=one, refrence_value=refrence_value)
+            ) 
+        return new_list
+    elif type(pure_value) is dict:
+        new_dict: dict[str, Any] = {{}}
+        for key_, value_ in pure_value.items(): #type: ignore
+            new_dict[key_] = convert_pure_dict_into_a_dict_that_has_enum_object( #type: ignore
+                pure_value=value_, 
+                refrence_value=refrence_value().property_name_to_its_type_dict_.get(key_)
+            ) #type: ignore
+        return new_dict
+    else:
+        if str(refrence_value).startswith("<enum"):
+            default_value = None
+            for temp_index, temp_value in enumerate(refrence_value(0)._member_names_):
+                if temp_value == pure_value:
+                    default_value = refrence_value(temp_index) 
+                    break
+            return default_value
+        else:
+            if refrence_value in _ygrpc_official_types:
+                return pure_value
+            else:
+                return None
+
+
+class YRPC_OBJECT_BASE_CLASS:
+    def to_dict(self, ignore_null: bool=False) -> dict[str, Any]:
+        new_dict = convert_dict_that_has_enum_object_into_pure_dict(value=self.__dict__.copy())
+        return new_dict.copy() #type: ignore
+
+    def from_dict(self, dict: dict[str, Any]):
+        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=dict.copy(), refrence_value=self.__class__)
+        old_self_dict = self.__dict__.copy() 
+        for key, value in new_dict.items():
+            if key in old_self_dict:
+                setattr(self, key, value)
+
+    def clone(self):
+        return self.create_a_new_instance_from_dict(self.to_dict()) 
+
+    def create_a_new_instance_from_dict(self, dict: dict[str, Any]):
+        an_object = self.__class__()
+        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=dict.copy(), refrence_value=an_object.__class__)
+        for key, value in new_dict.items():
+            if key in an_object.__dict__:
+                setattr(an_object, key, value)
+        return an_object
+
+        
+{dataclass_code_block_list_text}
+        """.strip()
+        return template_text.strip()
+
+    def generate_python_code(self, input_folder: str, input_files: list[str], output_folder: str = "src/generated_yrpc"):
+        """
+        input_folder: where protobuff files was located
+
+        input_files: it is a list, like ["english.proto", "pornhub.proto"]
+
+        output_folder: where those generated code file was located
+        """
+        input_folder = input_folder.rstrip("/")
+
+        if not disk.exists(input_folder):
+            raise Exception(f"The input_forder '{input_folder}' does not exist!")
+
+        if not disk.is_directory(input_folder):
+            raise Exception(f"The input_folder '{input_folder}' must be an directory.")
+
+        if not disk.is_directory(output_folder):
+            disk.create_a_folder(output_folder)
+
+        files = disk.get_files(input_folder, recursive=False, type_limiter=[".proto"])
+
+        new_files:list[str] = []
+        for file in files:
+            if any([one for one in input_files if file.endswith("/"+one)]):
+                new_files.append(file)
+        files = new_files.copy()
+
+        for file in files:
+            filename,_ = disk.get_stem_and_suffix_of_a_file(file)
+            target_file_path = disk.join_paths(output_folder, filename+".py")
+
+            source_code = io_.read(file_path=file)
+            final_code = self._convert_yrpc_code_into_python_code(source_code=source_code)
+
+            io_.write(file_path=target_file_path, content=final_code)
 
 
 if __name__ == "__main__":
     yrpc = YRPC()
+
+    # source_code = io_.read("./playground/develop/test_protobuff_code.proto")
+    # arguments_dict, rpc_dict = yrpc.get_information_from_yrpc_protocol_code(source_code=source_code)
+
+    # print(arguments_dict)
+    # print()
+    # print(rpc_dict)
+
+    yrpc.generate_python_code(
+        input_folder="/Users/yingshaoxo/CS/auto_everything/playground/develop",
+        input_files=["test_protobuff_code.proto"],
+        output_folder="/Users/yingshaoxo/CS/auto_everything/playground/develop/build"
+    )
 
     # grpc = GRPC()
     # grpc.generate_python_code(
--- playground/develop/generated_python_protobuff_class_example.py
+++ playground/develop/generated_python_protobuff_class_example.py
@@ -1,38 +1,9 @@
-"""
-"UserStatus": {
-    "OFFLINE": {
-        "type": "string",
-        "is_list": false,
-        "name": "OFFLINE",
-        "feature": ""
-    },
-    "ONLINE": {
-        "type": "string",
-        "is_list": false,
-        "name": "ONLINE",
-        "feature": ""
-    }
-},
-"hello_request": {
-    "name": {
-        "type": "string",
-        "is_list": false,
-        "name": "name",
-        "feature": ""
-    },
-    "user_status": {
-        "type": "UserStatus",
-        "is_list": false,
-        "name": "user_status",
-        "feature": ""
-    }
-},
-"""
-
-
 from dataclasses import dataclass
 from enum import Enum
 from typing import Any
+
+
+_ygrpc_official_types = [int, float, str, bool]
 
 
 class UserStatus(Enum):
@@ -40,95 +11,157 @@ class UserStatus(Enum):
     ONLINE = 1
 
 
-def convert_dict_that_has_enum_object_into_pure_dict(a_dict: dict[str, Any]) -> dict[str, Any]:
-    new_dict: dict[str, Any] = {}
-    for key, value in a_dict.items():
+def convert_dict_that_has_enum_object_into_pure_dict(value: Any) -> dict[str, Any] | list[Any] | Any:
+    if type(value) is list:
+        new_list: list[Any] = []
+        for one in value: #type: ignore
+            new_list.append(convert_dict_that_has_enum_object_into_pure_dict(value=one)) 
+        return new_list
+    elif type(value) is dict:
+        new_dict: dict[str, Any] = {}
+        for key_, value_ in value.items(): #type: ignore
+            new_dict[key_] = convert_dict_that_has_enum_object_into_pure_dict(value=value_) #type: ignore
+        return new_dict
+    else:
         if str(type(value)).startswith("<enum"):
-            new_dict[key] = value.name
+            return value.name
         else:
-            new_dict[key] = value
-    return new_dict
-
-
-def convert_pure_dict_into_a_dict_that_has_enum_object(pure_dict: dict[str, Any], target_dict: dict[str, Any], property_name_to_its_type_dict_: dict[str, Any]) -> dict[str, Any]:
-    for key, value in target_dict.items():
-        if key in pure_dict:
-            if str(type(value)).startswith("<enum"):
-                if key not in property_name_to_its_type_dict_:
-                    value = None
-                else:
-                    possible_value = pure_dict[key]
-                    the_enum_class = property_name_to_its_type_dict_[key]
-                    for temp_index, temp_value in enumerate(the_enum_class._member_names_):
-                        if temp_value == possible_value:
-                            value = the_enum_class(temp_index) 
-                            break
-
-                target_dict[key] = value
+            if type(value) in _ygrpc_official_types:
+                return value
             else:
-                target_dict[key] = pure_dict[key]
-    return target_dict
+                # handle custom message data type
+                if value == None:
+                    return None
+                else:
+                    return convert_dict_that_has_enum_object_into_pure_dict(
+                        value=value.to_dict()
+                    )
+
+
+def convert_pure_dict_into_a_dict_that_has_enum_object(pure_value: Any, refrence_value: Any) -> Any:
+    if type(pure_value) is list:
+        new_list: list[Any] = []
+        for one in pure_value: #type: ignore
+            new_list.append(
+                convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=one, refrence_value=refrence_value)
+            ) 
+        return new_list
+    elif type(pure_value) is dict:
+        new_dict: dict[str, Any] = {}
+        for key_, value_ in pure_value.items(): #type: ignore
+            new_dict[key_] = convert_pure_dict_into_a_dict_that_has_enum_object( #type: ignore
+                pure_value=value_, 
+                refrence_value=refrence_value().property_name_to_its_type_dict_.get(key_)
+            ) #type: ignore
+        return new_dict
+    else:
+        if str(refrence_value).startswith("<enum"):
+            default_value = None
+            for temp_index, temp_value in enumerate(refrence_value(0)._member_names_):
+                if temp_value == pure_value:
+                    default_value = refrence_value(temp_index) 
+                    break
+            return default_value
+        else:
+            if refrence_value in _ygrpc_official_types:
+                return pure_value
+            else:
+                return None
+
+
+class YRPC_OBJECT_BASE_CLASS:
+    def to_dict(self, ignore_null: bool=False) -> dict[str, Any]:
+        new_dict = convert_dict_that_has_enum_object_into_pure_dict(value=self.__dict__.copy())
+        return new_dict.copy() #type: ignore
+
+    def from_dict(self, dict: dict[str, Any]):
+        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=dict.copy(), refrence_value=self.__class__)
+        old_self_dict = self.__dict__.copy() 
+        for key, value in new_dict.items():
+            if key in old_self_dict:
+                setattr(self, key, value)
+
+    def clone(self):
+        return self.create_a_new_instance_from_dict(self.to_dict()) 
+
+    def create_a_new_instance_from_dict(self, dict: dict[str, Any]):
+        an_object = self.__class__()
+        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_value=dict.copy(), refrence_value=an_object.__class__)
+        for key, value in new_dict.items():
+            if key in an_object.__dict__:
+                setattr(an_object, key, value)
+        return an_object
 
 
 @dataclass()
-class hello_request:
+class Yingshaoxo_info(YRPC_OBJECT_BASE_CLASS):
     name: str | None = None
-    user_status: UserStatus | None = None
+    age: int | None = None
+    sex: str | None = None
+    super_power: bool | None = None
+
     property_name_to_its_type_dict_ = {
         "name": str,
-        "user_status": UserStatus
+        "age": int,
+        "sex": str,
+        "super_power": bool
+    }
+
+    @dataclass()
+    class key_string_dict_:
+        name: str = "name"
+        age: str = "age"
+        sex: str = "sex"
+        super_power: str = "super_power"
+    
+
+@dataclass()
+class hello_request(YRPC_OBJECT_BASE_CLASS):
+    name: str | None = None
+    user_status: UserStatus | None = None
+    user_status_list: list[UserStatus] | None = None
+    yingshaoxo_info: Yingshaoxo_info | None = None
+
+    property_name_to_its_type_dict_ = {
+        "name": str,
+        "user_status": UserStatus,
+        "user_status_list": UserStatus,
+        "yingshaoxo_info": Yingshaoxo_info
     }
 
     @dataclass()
     class key_string_dict_:
         name: str = "name"
         user_status: str = "user_status"
-
-    def to_dict(self, ignore_null: bool=False) -> dict[str, Any]:
-        new_dict = convert_dict_that_has_enum_object_into_pure_dict(a_dict=self.__dict__)
-        return new_dict
-
-    def clone(self):
-        return self.create_a_new_instance_from_dict(self.to_dict()) 
-
-    def from_dict(self, dict: dict[str, Any]):
-        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_dict=dict, target_dict=self.__dict__, property_name_to_its_type_dict_=self.property_name_to_its_type_dict_)
-        old_self_dict = self.__dict__.copy() 
-        for key, value in new_dict.items():
-            if key in old_self_dict:
-                setattr(self, key, value)
-
-    @staticmethod
-    def create_a_new_instance_from_dict(dict: dict[str, Any]):
-        an_object = hello_request()
-        new_dict = convert_pure_dict_into_a_dict_that_has_enum_object(pure_dict=dict, target_dict=an_object.__dict__, property_name_to_its_type_dict_=an_object.property_name_to_its_type_dict_)
-        for key, value in new_dict.items():
-            if key in an_object.__dict__:
-                setattr(an_object, key, value)
-        return an_object
+        user_status_list: str = "user_status_list"
+        yingshaoxo_info: str = "yingshaoxo_info"
     
 
-
 if __name__ == "__main__":
-    object1 = hello_request(name="a", user_status=UserStatus.OFFLINE)
+    object1 = hello_request(name="a", 
+                            user_status=UserStatus.OFFLINE, 
+                            user_status_list=[UserStatus.OFFLINE, UserStatus.ONLINE],
+                            yingshaoxo_info=Yingshaoxo_info(name="yingshaoxo", age=24, sex="male", super_power=True)
+                            )
     object2 = hello_request(name="b", user_status=UserStatus.ONLINE)
+
+    # print(object1.user_status_list)
+    # print(type(object1.user_status_list))
 
     object1_dict = object1.to_dict()
     object2_dict = object2.to_dict()
 
     print(object1_dict)
-    print(object2_dict)
+    # print(object2_dict)
 
     print("---------")
 
-    object1.from_dict(object2_dict)
+    print(object2.to_dict())
     object2.from_dict(object1_dict)
-
-    print(object1.to_dict())
     print(object2.to_dict())
 
-    print("---------")
+    # print("---------")
 
-    print(object1.clone())
+    # print(hello_request.create_a_new_instance_from_dict(object2.to_dict()))
 
 
