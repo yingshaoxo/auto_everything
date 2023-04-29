aa4db3593b9f45b97256c2575d62914c2324c6c2
yingshaoxo <yingshaoxo@gmail.com>
2023-04-29 12:57:35 +0800

    update find_big_file() function

--- example/terminal/Tools.py
+++ example/terminal/Tools.py
@@ -272,7 +272,7 @@ bfg --delete-files {filename}
     
     def find_big_file(self, path: str = "."):
         t.run(f"""
-        du -h --max-depth=1 {path}
+        du -h --max-depth=1 {path} | sort -h
         """)
 
     def hi(self):
