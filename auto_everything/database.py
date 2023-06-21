fa22bf552d420ee5a2fb481316267e732f150ada
yingshaoxo <yingshaoxo@gmail.com>
2023-06-21 11:48:58 +0800

    upgrade the yingshaoxo database rpc generator code

--- auto_everything/database.py
+++ auto_everything/database.py
@@ -562,7 +562,7 @@ class Database_Of_Yingshaoxo:
         for line in [line.strip() for line in text.split("\n") if line.strip() != ""]:
             if not line.startswith('#'):
                 lines.append(line)
-        self._io.write(self.database_txt_file_path, "\n".join(lines))
+        self._io.write(self.database_txt_file_path, "\n".join(lines)+"\n")
 
     def clear_database(self):
         self._io.write(self.database_txt_file_path, "")
@@ -611,98 +611,13 @@ class Yingshaoxo_Database_{variable_type}:
         self.database_of_yingshaoxo.add(data=item.to_dict())
 
     def search(self, item_filter: {variable_type}, page_number:int|None=None, page_size:int|None=None, start_from:int=0, reverse:bool=False):
-        self._raw_search_counting = 0
-        self._search_counting = 0
-        if (page_number!=None and page_size != None and start_from != None):
-            self._real_start = page_number * page_size
-            self._real_end = self._real_start + page_size
-
-        item_dict = item_filter.to_dict()
-
-        def one_row_dict_filter(a_dict_: dict[str, Any]):
-            self._raw_search_counting += 1
-
-            if (page_number!=None and page_size != None and start_from != None):
-                if self._raw_search_counting < start_from:
-                    return None
-                if self._search_counting < self._real_start:
-                    return None
-                if self._search_counting > self._real_end:
-                    return None
-
-            result = True
-            for key, value in item_dict.items():
-                if value == None:
-                    # ignore None value because it is not defined
-                    continue
-                if key not in a_dict_.keys():
-                    result = False
-                    break
-                else:
-                    value2 = a_dict_.get(key)
-                    if value == value2:
-                        continue
-                    else:
-                        result = False
-                        break
-
-            if result == True:
-                self._search_counting += 1
-                return a_dict_
-            else:
-                return None
-        return self.database_of_yingshaoxo.search(one_row_dict_handler=one_row_dict_filter)
+        return _search_function(self=self, item_filter=item_filter, page_number=page_number, page_size=page_size, start_from=start_from, reverse=reverse)
 
     def delete(self, item_filter: {variable_type}):
-        item_dict = item_filter.to_dict()
-        def one_row_dict_filter(a_dict_: dict[str, Any]):
-            result = True
-            for key, value in item_dict.items():
-                if value == None:
-                    # ignore None value because it is not defined
-                    continue
-                if key not in a_dict_.keys():
-                    result = False
-                    break
-                else:
-                    value2 = a_dict_.get(key)
-                    if value == value2:
-                        continue
-                    else:
-                        result = False
-                        break
-            return result
-        self.database_of_yingshaoxo.delete(one_row_dict_filter=one_row_dict_filter)
+        return _delete(self=self, item_filter=item_filter)
     
     def update(self, old_item_filter: {variable_type}, new_item: {variable_type}):
-        item_dict = old_item_filter.to_dict()
-        def one_row_dict_handler(a_dict_: dict[str, Any]):
-            result = True
-            for key, value in item_dict.items():
-                if value == None:
-                    # ignore None value because it is not defined
-                    continue
-                if key not in a_dict_.keys():
-                    result = False
-                    break
-                else:
-                    value2 = a_dict_.get(key)
-                    if value == value2:
-                        continue
-                    else:
-                        result = False
-                        break
-            if result == True:
-                new_object = {{
-                    key:value for key, value
-                    in new_item.to_dict().items()
-                    if value != None
-                }}
-                a_dict_.update(new_object)
-                return a_dict_
-            else:
-                return None
-        self.database_of_yingshaoxo.update(one_row_dict_handler=one_row_dict_handler)
+        return _update(self=self, old_item_filter=old_item_filter, new_item=new_item)
                     """.rstrip().lstrip('\n'))
 
                 variable_list = list(set([input_variable, output_variable]))
@@ -718,6 +633,110 @@ class Yingshaoxo_Database_{variable_type}:
             template_text = f"""
 from .{identity_name}_objects import *
 from auto_everything.database import Database_Of_Yingshaoxo
+
+
+def _search_function(self: Any, item_filter: Any, page_number:int|None=None, page_size:int|None=None, start_from:int=0, reverse:bool=False):
+    search_temp_dict = {{}}
+    search_temp_dict["_raw_search_counting"] = 0
+    search_temp_dict["_search_counting"] = 0
+    if (page_number!=None and page_size != None and start_from != None):
+        search_temp_dict["_real_start"] = page_number * page_size
+        search_temp_dict["_real_end"] = search_temp_dict["_real_start"] + page_size
+
+    item_dict = item_filter.to_dict()
+
+    def one_row_dict_filter(a_dict_: dict[str, Any]):
+        search_temp_dict["_raw_search_counting"] += 1
+
+        if (page_number!=None and page_size != None and start_from != None):
+            if search_temp_dict["_raw_search_counting"] < start_from:
+                return None
+
+        result = True
+        for key, value in item_dict.items():
+            if value == None:
+                # ignore None value because it is not defined
+                continue
+            if key not in a_dict_.keys():
+                result = False
+                break
+            else:
+                value2 = a_dict_.get(key)
+                if value == value2:
+                    continue
+                else:
+                    result = False
+                    break
+
+        final_result = None
+        if result == True:
+            search_temp_dict["_search_counting"] += 1
+            final_result = a_dict_
+        else:
+            final_result = None
+
+        if (page_number!=None and page_size != None and start_from != None):
+            if search_temp_dict["_search_counting"] <= search_temp_dict["_real_start"]:
+                return None
+            if search_temp_dict["_search_counting"] > search_temp_dict["_real_end"]:
+                return None
+        
+        return final_result
+
+    return self.database_of_yingshaoxo.search(one_row_dict_handler=one_row_dict_filter)
+
+
+def _delete(self, item_filter: Any):
+    item_dict = item_filter.to_dict()
+    def one_row_dict_filter(a_dict_: dict[str, Any]):
+        result = True
+        for key, value in item_dict.items():
+            if value == None:
+                # ignore None value because it is not defined
+                continue
+            if key not in a_dict_.keys():
+                result = False
+                break
+            else:
+                value2 = a_dict_.get(key)
+                if value == value2:
+                    continue
+                else:
+                    result = False
+                    break
+        return result
+    self.database_of_yingshaoxo.delete(one_row_dict_filter=one_row_dict_filter)
+
+
+def _update(self, old_item_filter: Any, new_item: Any):
+    item_dict = old_item_filter.to_dict()
+    def one_row_dict_handler(a_dict_: dict[str, Any]):
+        result = True
+        for key, value in item_dict.items():
+            if value == None:
+                # ignore None value because it is not defined
+                continue
+            if key not in a_dict_.keys():
+                result = False
+                break
+            else:
+                value2 = a_dict_.get(key)
+                if value == value2:
+                    continue
+                else:
+                    result = False
+                    break
+        if result == True:
+            new_object = {{
+                key:value for key, value
+                in new_item.to_dict().items()
+                if value != None
+            }}
+            a_dict_.update(new_object)
+            return a_dict_
+        else:
+            return None
+    self.database_of_yingshaoxo.update(one_row_dict_handler=one_row_dict_handler)
 
 
 {database_class_list_text}
