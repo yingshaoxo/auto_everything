c010d14e030d84ba5fe4040d168d7f08db4ce657
yingshaoxo <yingshaoxo@gmail.com>
2023-09-12 17:40:04 +0800

    add Yingshaoxo_Text_to_Speech

--- auto_everything/ml.py
+++ auto_everything/ml.py
@@ -1,5 +1,6 @@
 from typing import Any
 import random
+import os
 
 from auto_everything.terminal import Terminal
 from auto_everything.disk import Disk, Store
@@ -512,6 +513,115 @@ class Yingshaoxo_Translator():
 
     def english_to_chinese(self, text: str, sentence_seperation: bool = False):
         return self.translate(text=text, from_language=self.languages.ENGLISH, to_language=self.languages.CHINESE, sentence_seperation=sentence_seperation)
+
+
+class Yingshaoxo_Text_to_Speech():
+    def __init__(self):
+        #pip install TTS
+        #sudo apt install ffmpeg                 or          https://github.com/markus-perl/ffmpeg-build-script#:~:text=maintain%20different%20systems.-,Installation,-Quick%20install%20and
+        os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+        from TTS.api import TTS
+        self.TTS = TTS
+
+        from auto_everything.terminal import Terminal
+        from auto_everything.disk import Disk
+        self.terminal = Terminal()
+        self.disk = Disk()
+
+        import torch
+        use_gpu = True if torch.cuda.is_available() else False
+        self.torch = torch
+        #pprint(TTS.list_models())
+
+        self.tts_en = TTS("tts_models/en/ljspeech/tacotron2-DDC", gpu=use_gpu)
+        #tts_en = TTS("tts_models/en/ljspeech/fast_pitch", gpu=use_gpu)
+        self.tts_cn = TTS("tts_models/zh-CN/baker/tacotron2-DDC-GST", gpu=use_gpu)
+
+    def _language_splitor(self, text: str):
+        language_list = []
+        index = 0
+        while True:
+            temp_string = ""
+            if (index >= len(text)):
+                break
+            char = text[index]
+            while ord(char) < 128:
+                # english
+                char = text[index]
+                temp_string += char
+                index += 1
+                if (index >= len(text)):
+                    break
+            if (temp_string.strip() != ""):
+                temp_string = temp_string[:-1]
+                index -= 1
+                language_list.append({
+                    "language": "en",
+                    "text": temp_string
+                })
+
+            temp_string = ""
+            if (index >= len(text)):
+                break
+            char = text[index]
+            while not ord(char) < 128:
+                # chinese 
+                char = text[index]
+                temp_string += char
+                index += 1
+                if (index >= len(text)):
+                    break
+            if (temp_string.strip() != ""):
+                temp_string = temp_string[:-1]
+                index -= 1
+                language_list.append({
+                    "language": "cn",
+                    "text": temp_string
+                })
+
+            if (index+1 >= len(text)):
+                break
+
+        if len(language_list) > 0:
+            language_list[-1]["text"] += text[-1]
+        
+        new_list = []
+        for index, one in enumerate(language_list):
+            new_text = language_list[index]["text"].strip()
+            if len(new_text) > 0:
+                new_list.append({
+                    'language': one['language'],
+                    'text': new_text
+                })
+
+        return new_list
+
+    def _speak_it(self, language: str, text: str):
+        output_file = os.path.abspath(os.path.join(self.disk.get_a_temp_folder_path(), "output.wav"))
+        self.disk.create_a_folder(self.disk.get_directory_path(output_file))
+
+        if (language == "en"):
+            tts = self.tts_en
+        else:
+            tts = self.tts_cn
+
+        if tts.speakers == None:
+            tts.tts_to_file(text=text, file_path=output_file)
+        else:
+            tts.tts_to_file(text=text, file_path=output_file, speaker=tts.speakers[0], language=tts.languages[0], speed=2.5) #type:ignore
+
+        self.terminal.run(f"""
+        ffplay -autoexit -nodisp "{output_file}"
+                """, wait=True)
+        
+        self.disk.delete_a_file(output_file)
+
+    def speak_it(self, text: str):
+        data_ = self._language_splitor(text)
+        for one in data_:
+            print(one)
+            self._speak_it(language=one["language"], text=one["text"])
 
 
 if __name__ == "__main__":
