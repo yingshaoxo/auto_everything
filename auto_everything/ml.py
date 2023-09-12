883356353878867242153725f0e95d00a664fdbc
yingshaoxo <yingshaoxo@gmail.com>
2023-09-12 15:47:37 +0800

    add Yingshaoxo_Translator

--- auto_everything/ml.py
+++ auto_everything/ml.py
@@ -468,5 +468,51 @@ class Yingshaoxo_Speech_Recognizer():
             return ""
 
 
+class Yingshaoxo_Translator():
+    def __init__(self):
+        # pip install dl-translate
+        import dl_translate
+        from auto_everything.language import Language
+        self.dl_translate = dl_translate
+        self.dl_translate_model = self.dl_translate.TranslationModel(device="auto")
+        self.languages = self.dl_translate.lang
+        self._language = Language()
+    
+    def translate(self, text: str, from_language: Any, to_language: Any, sentence_seperation: bool = False):
+        try:
+            text = text.strip()
+            if sentence_seperation == True:
+                data_list = self._language.seperate_text_to_segments(text=text, ignore_space=True)
+                """
+                [
+                    {
+                        "is_punctuation_or_space": true, "text": "?",
+                    }, {
+                        "is_punctuation_or_space": false, "text": "Yes",
+                    },
+                ]
+                """
+                text_list = []
+                for segment in data_list:
+                    if segment["is_punctuation_or_space"] == False:
+                        result = self.dl_translate_model.translate(segment["text"], source=from_language, target=to_language)
+                        result = str(result).strip("!\"#$%&'()*+, -./:;<=>?@[\\]^_`{|}~ \n，。！？；：（）［］【】")
+                        text_list.append(result)
+                    else:
+                        text_list.append(segment["text"])
+                return "".join(text_list)
+            else:
+                return self.dl_translate_model.translate(input_text, source=from_language, target=to_language)
+        except Exception as e:
+            print(e)
+            return text
+    
+    def chinese_to_english(self, text: str, sentence_seperation: bool = False):
+        return self.translate(text=text, from_language=self.languages.CHINESE, to_language=self.languages.ENGLISH, sentence_seperation=sentence_seperation)
+
+    def english_to_chinese(self, text: str, sentence_seperation: bool = False):
+        return self.translate(text=text, from_language=self.languages.ENGLISH, to_language=self.languages.CHINESE, sentence_seperation=sentence_seperation)
+
+
 if __name__ == "__main__":
     pass
