aaa6f9c3cdfe855bb5e5b0b09d82424a33d7de35
yingshaoxo <yingshaoxo@gmail.com>
2023-10-09 13:03:09 +0800

    update example

--- example/http/http_example.py
+++ example/http/http_example.py
@@ -1,18 +1,18 @@
 from auto_everything.http import Yingshaoxo_Http_Server, Yingshaoxo_Http_Request
 
 
-def home_handler_example(request: Yingshaoxo_Http_Request) -> dict:
+def home_handler(request: Yingshaoxo_Http_Request) -> dict:
     return {"message": "Hello, world, fight for inner peace."}
 
-def special_handler_example(request: Yingshaoxo_Http_Request) -> str:
+def special_handler(request: Yingshaoxo_Http_Request) -> str:
     return "Hello, world, fight for personal freedom."
 
 
-router_example = {
-    r"(.*)": home_handler_example,
-    r"/freedom": special_handler_example
+router = {
+    r"(.*)": home_handler,
+    r"/freedom": special_handler
 }
 
 
-yingshaoxo_http_server = Yingshaoxo_Http_Server(router=router_example)
+yingshaoxo_http_server = Yingshaoxo_Http_Server(router=router)
 yingshaoxo_http_server.start(host="0.0.0.0", port=1212)
