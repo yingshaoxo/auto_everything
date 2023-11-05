e0400058df8f6e4dd21ed4edebfef3d0a1a8dc79
yingshaoxo <yingshaoxo@gmail.com>
2023-11-05 09:11:05 +0800

    update context based chat example

--- example/ml/context_based_chat.py
+++ example/ml/context_based_chat.py
@@ -5,6 +5,66 @@ ml = ML()
 disk = Disk()
 terminal = Terminal()
 
+text_generator = ml.Yingshaoxo_Text_Generator(
+    input_txt_folder_path="../18.fake_ai_asistant/input_txt_files",
+    use_machine_learning=False
+)
+
+def decode_response(text: str, chat_context: str):
+    splits = text.split("\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n")
+    if (len(splits) > 1):
+        response = splits[1].strip()
+    elif (len(splits) == 1):
+        response = splits[0].strip()
+    else:
+        response = ""
+    new_code = f"""
+chat_context = '''{chat_context}'''
+
+{response}
+"""
+    final_response = terminal.run_python_code(code=new_code)
+    if final_response.strip() == "":
+        final_response = response
+    final_response = "\n".join([one for one in final_response.split("\n") if not one.strip().startswith("__**")])
+    return final_response
+
+
+print("\n\n")
+all_input_text = ""
+
+while True:
+    input_text = input("What you want to say? \n")
+
+    all_input_text += input_text + "\n"
+    real_input = all_input_text[-800:].strip()
+
+    response = text_generator.search_and_get_following_text_in_a_exact_way(input_text=real_input, quick_mode=True)
+    response = decode_response(text=response, chat_context=all_input_text)
+
+    print("\n\n---------\n\n")
+    print(response)
+    print("\n\n---------\n\n")
+
+    #global_string_corrector_dict = text_generator.get_global_string_word_based_corrector_dict_by_using_yingshaoxo_method(all_input_text, levels=2)
+    #new_response = text_generator.correct_sentence_based_on_word_by_using_yingshaoxo_method(response, global_string_corrector_dict=global_string_corrector_dict)
+    #print("\n**********\n", new_response,"\n************\n")
+
+
+
+#######
+# If you want to have a less accurate one, you use the following version
+#######
+
+
+"""
+from auto_everything.ml import ML
+from auto_everything.disk import Disk
+from auto_everything.terminal import Terminal
+ml = ML()
+disk = Disk()
+terminal = Terminal()
+
 text_generator =  ml.Yingshaoxo_Text_Generator()
 
 text = text_generator.get_source_text_data_by_using_yingshaoxo_method(input_txt_folder_path="../18.fake_ai_asistant/input_txt_files")
@@ -19,52 +79,4 @@ while True:
         print(splits[1])
     else:
         print(splits[0])
-
-
-#######
-# If you want to have a more accurate one, you use the following version
-#######
-
-
-#yingshaoxo_text_generator = ml.Yingshaoxo_Text_Generator(
-#    input_txt_folder_path="../18.fake_ai_asistant/input_txt_files",
-#    use_machine_learning=False
-#)
-#
-#def decode_response(text: str, chat_context: str):
-#    #print("`"+text+"`")
-#    splits = text.split("\n\n__**__**__yingshaoxo_is_the_top_one__**__**__\n\n")
-#    if (len(splits) > 1):
-#        response = splits[1].strip()
-#    elif (len(splits) == 1):
-#        response = splits[0].strip()
-#    else:
-#        response = ""
-#    new_code = f"""
-#chat_context = '''{chat_context}'''
-#
-#{response}
-#"""
-#    final_response = terminal.run_python_code(code=new_code)
-#    if final_response.strip() == "":
-#        final_response = response
-#    final_response = "\n".join([one for one in final_response.split("\n") if not one.strip().startswith("__**")])
-#    return final_response
-#
-#
-#print("\n\n")
-#all_input_text = ""
-#
-#while True:
-#    input_text = input("What you want to say? \n")
-#
-#    all_input_text += input_text + "\n"
-#    real_input = all_input_text[-800:].strip()
-#    response = yingshaoxo_text_generator.search_and_get_following_text_in_a_exact_way(input_text=real_input, quick_mode=True)
-#    response = decode_response(text=response, chat_context=all_input_text)
-#
-#    all_input_text += response + "\n"
-#
-#    print("\n\n---------\n\n")
-#    print(response)
-#    print("\n\n---------\n\n")
+"""
