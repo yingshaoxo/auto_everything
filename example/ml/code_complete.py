ae6baf716cdd64f626afb44bd36493a87b6d2e1c
yingshaoxo <yingshaoxo@gmail.com>
2023-11-06 07:47:39 +0800

    update code

--- example/ml/code_complete.py
+++ example/ml/code_complete.py
@@ -11,7 +11,7 @@ global_string_dict = None
 while True:
     input_text = input("What you want to say? ")
     #if global_string_dict == None:
-    #    text_generator.get_global_string_dict_by_using_yingshaoxo_method(source_text_data=text, levels=10)
+    #    global_string_dict = text_generator.get_global_string_dict_by_using_yingshaoxo_method(source_text_data=text, levels=10)
     #result = text_generator.get_next_x_chars_by_using_yingshaoxo_method(input_text, x=30, global_string_dict=global_string_dict)
     result = text_generator.next_code_generation(data_source_text=text, input_text=input_text, type_limiter=[".py"], how_long_the_text_you_want_to_get=30)
     print(result)
