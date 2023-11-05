8ffd6118d25a06e9870c5ffaac05e4994b6cc19c
yingshaoxo <yingshaoxo@gmail.com>
2023-11-05 11:07:58 +0800

    add correct_sentence_by_using_yingshaoxo_regex_method function

--- auto_everything/ml.py
+++ auto_everything/ml.py
@@ -464,6 +464,45 @@ class Yingshaoxo_Text_Generator():
             break
         return new_text
 
+    def correct_sentence_by_using_yingshaoxo_regex_method(self, input_text: str, source_data_text: str, level: int=3) -> str:
+        import re
+
+        def find_match_string_in_source_data(before_chars: str, after_chars: str):
+            before_chars = re.escape(before_chars)
+            after_chars = re.escape(after_chars)
+            result_list = re.findall(pattern=f"{before_chars}(.){after_chars}", string=source_data_text)
+            counting_dict = {}
+            for one in result_list:
+                if one in counting_dict.keys():
+                    counting_dict[one] += 1
+                else:
+                    counting_dict[one] = 1
+            items = list(counting_dict.items())
+            items.sort(key=lambda item: item[1], reverse=True)
+            if len(items) > 0:
+                return items[0][0]
+            else:
+                return None
+
+        new_text = ""
+        for index, _ in enumerate(input_text):
+            if index < (level-1):
+                new_text += input_text[index]
+                continue
+            if index >= len(input_text) - level:
+                new_text += input_text[index]
+                continue
+            before_chars = input_text[index-level: index]
+            after_chars = input_text[index+1: index+1+level]
+            #print(before_chars, input_text[index], after_chars)
+            new_chars = find_match_string_in_source_data(before_chars, after_chars)
+            if new_chars != None:
+                new_text += new_chars
+            else:
+                new_text += input_text[index]
+
+        return new_text
+
     def get_global_string_word_based_corrector_dict_by_using_yingshaoxo_method(self, source_text_data: str, levels: int = 10):
         global_string_dict = {}
 
