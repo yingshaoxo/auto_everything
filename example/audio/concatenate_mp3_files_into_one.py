ac6bd684192c17d71f1a34a2381e59bfa82a4f5a
yingshaoxo <yingshaoxo@gmail.com>
2023-11-28 11:16:07 +0800

    add merge mp3 audio script

--- example/audio/concatenate_mp3_files_into_one.py
+++ example/audio/concatenate_mp3_files_into_one.py
@@ -2,8 +2,8 @@ from auto_everything.disk import Disk
 disk = Disk()
 from pydub import AudioSegment
 
-input_folder = ""
-output_mp3_file = "~/Downloads/one.mp3"
+input_folder = "/home/yingshaoxo/Disk/Sync_Folder/Yingshaoxo_Data/Additional/Music/一只怪兽叭"
+output_mp3_file = disk._expand_user("~/Downloads/one.mp3")
 
 if not disk.exists(input_folder):
     print(f"You have to give me a right mp3 input folder than '{input_folder}'")
@@ -17,7 +17,8 @@ for index, file in enumerate(files):
     if index == 0:
         sound = AudioSegment.from_mp3(file)
         continue
-    sound = sound.append(AudioSegment.from_mp3(file), crossfade=1500)
+    sound = sound.append(AudioSegment.silent(duration=10000))#, crossfade=1500)
+    sound = sound.append(AudioSegment.from_mp3(file))#, crossfade=1500)
 
 sound.export(output_mp3_file, format="mp3")
 print("Done.")
