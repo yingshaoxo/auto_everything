f37ee01d852b4454bdd14a361b195f854a4e6978
yingshaoxo <yingshaoxo@gmail.com>
2024-06-05 09:11:07 -0400

    add a note for mp3 audio compression

--- example/audio/convert_wav_to_mp3_size_wav.py
+++ example/audio/convert_wav_to_mp3_size_wav.py
@@ -8,3 +8,5 @@ audio = audio.get_simplified_audio()
 audio.write_wav_file("/home/yingshaoxo/Downloads/song_small.wav")
 audio.save_to_file("/home/yingshaoxo/Downloads/song_small.wav.txt")
 
+print("You should get a 3.3MB wav file, but if you convert the output wav file to mp3, it could be smaller, for example, 200KB for a song.")
+
