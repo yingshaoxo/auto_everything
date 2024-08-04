4e3804ffa3e48b077507711047e824b93c0cbfcd
yingshaoxo <yingshaoxo@gmail.com>
2024-08-04 00:11:53 -0400

    fix more error on http client example

--- example/http/broswer.py
+++ example/http/broswer.py
@@ -5,4 +5,5 @@ Should use Yingshaoxo_Http_Client as the http client, use image module GUI as fr
 from auto_everything.http_ import Yingshaoxo_Http_Client
 yingshaoxo_http_client = Yingshaoxo_Http_Client()
 
+print(yingshaoxo_http_client.get("http://127.0.0.1:1212/"))
 print(yingshaoxo_http_client.post("http://127.0.0.1:1212/post", {"hi": "you"}))
