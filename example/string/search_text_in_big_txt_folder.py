7b54be04056dd6d66ba98671ac07bfbcf0bea480
yingshaoxo <yingshaoxo@gmail.com>
2025-07-04 16:02:19 +0800

    make some changes to show more content in the text search under folder

--- example/string/search_text_in_big_txt_folder.py
+++ example/string/search_text_in_big_txt_folder.py
@@ -23,12 +23,28 @@ source_text_list = [one.strip() for one in source_text.split("\n") if one.strip(
 
 sample_numbers = 1000000
 
+the_index = 0
 while True:
     print("\n\n\n------------\n\n\n")
-    input_text = input("What you want to search? ")
+    input_text = input("What you want to search? ").strip()
+    if input_text == "n":
+        next_lines = "\n".join(source_text_list[the_index:the_index+10])
+        print(next_lines)
+        the_index = the_index + 10
+        continue
 
     sampled_list = random.sample(source_text_list, sample_numbers) if len(source_text_list) >= sample_numbers else random.choices(source_text_list, k=sample_numbers)
 
     result_text = string.search_text_in_text_list_by_using_long_sub_sentence_and_only_return_one_text(input_text, sampled_list)
+    next_lines = ""
+    for index, one in enumerate(source_text_list):
+        if one == result_text:
+            next_lines = "\n".join(source_text_list[index+1:index+10])
+            the_index = index + 10
 
-    print(result_text)
+    #try:
+    #    print(input_text + result_text.split(input_text[-3:])[-1] + next_lines)
+    #except Exception as e:
+    #    print(result_text + next_lines)
+
+    print(result_text + next_lines)
