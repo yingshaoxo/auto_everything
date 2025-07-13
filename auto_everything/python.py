8da4e14a8b1a2da92e4b3ccd483547262c003f1b
yingshaoxo <yingshaoxo@gmail.com>
2025-07-13 22:26:42 +0800

    fix a type bug for python3.2

--- auto_everything/python.py
+++ auto_everything/python.py
@@ -666,7 +666,7 @@ class Python():
         else:
             data = copy.deepcopy(data)
 
-            def infinite_loop(the_data: Any) -> Any:
+            def infinite_loop(the_data):
                 the_type = type(the_data)
                 if the_type == str:
                     return the_data[:limit] + "..."
