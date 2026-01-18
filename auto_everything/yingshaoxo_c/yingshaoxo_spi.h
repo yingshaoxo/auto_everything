26813cf7d40b9d4a3700de0a0d74c4f14fe9f2fb
yingshaoxo <yingshaoxo@gmail.com>
2026-01-18 21:12:51 +0800

    c: made my own SPI algorithm, both c and python

---
+++ auto_everything/yingshaoxo_c/yingshaoxo_spi.h
@@ -0,0 +1,297 @@
+/* tested in arduino nano v3 (no wifi version, 2k memory, 30kb flash) */ 
+
+#ifndef yingshaoxo_spi
+#define yingshaoxo_spi
+
+// fake spi input: d4(clock), d5(data)
+#define yingshaoxo_spi_input_clock_pin 4
+#define yingshaoxo_spi_input_data_pin 5
+unsigned char yingshaoxo_spi_input_that_transmit_ends_with_04[68] = { '\0' };
+unsigned int yingshaoxo_spi_input_has_been_initialized = 0;
+void set_up_yingshaoxo_spi_input() {
+    pinMode(13, OUTPUT); // LED
+    pinMode(yingshaoxo_spi_input_clock_pin, INPUT);
+    pinMode(yingshaoxo_spi_input_data_pin, INPUT);
+    yingshaoxo_spi_input_has_been_initialized = 1;
+}
+int get_yingshaoxo_spi_input_binary_0_or_1() {
+    int clock_value = 1;
+    while (clock_value == 1) {
+        clock_value = digitalRead(yingshaoxo_spi_input_clock_pin);
+    }
+    while (clock_value == 0) {
+        clock_value = digitalRead(yingshaoxo_spi_input_clock_pin);
+    }
+    return digitalRead(yingshaoxo_spi_input_data_pin);
+}
+unsigned char get_yingshaoxo_spi_input_single_byte() {
+    unsigned char a_byte = 0x00;
+    int i = 7;
+    for (i; i>=0; i--) {
+        a_byte |= (get_yingshaoxo_spi_input_binary_0_or_1() << i);
+    }
+    return a_byte;
+}
+int get_yingshaoxo_spi_input_bytes(unsigned char starting_byte, unsigned char end_byte) {
+    unsigned char a_byte = get_yingshaoxo_spi_input_single_byte();
+    int start = 0;
+    int i = 0;
+    while (1) {
+        if (start == 0) {
+            while (1) {
+                if (a_byte == starting_byte) {
+                    start = 1;
+                    break;
+                }
+                a_byte = get_yingshaoxo_spi_input_single_byte();
+            }
+        } else {
+            a_byte = get_yingshaoxo_spi_input_single_byte();
+            yingshaoxo_spi_input_that_transmit_ends_with_04[i] = a_byte;
+            if (a_byte == end_byte) {
+                yingshaoxo_spi_input_that_transmit_ends_with_04[i] = '\0';
+                break;
+            }
+            if (i >= 63) {
+                yingshaoxo_spi_input_that_transmit_ends_with_04[64] = '\0';
+                break;
+            }
+            i += 1;
+        }
+    }
+    return i;
+}
+int get_yingshaoxo_spi_input(unsigned long timeout) {
+    // the other side will send 0x01 as "00000001", which means this client will first receive 0, ends with 1.
+    /*
+    >>> io.bytes_to_binary_zero_and_one(bytes([0x00 | (1 << 0x07)]))
+    ['10000000']
+    >>> io.bytes_to_binary_zero_and_one(bytes([0x00 | (1 << 0x06)]))
+    ['01000000']
+    >>> io.bytes_to_binary_zero_and_one(bytes([0x00 | (1 << 0x05)]))
+    ['00100000']
+    */
+    if (yingshaoxo_spi_input_has_been_initialized == 0) {
+        // call set_up_yingshaoxo_spi_input() first
+        return 0;
+    }
+
+    unsigned long start_time = millis(); //millisecond, 1000 = 1 second
+    int index = 0;
+    int clock_value = 0;
+    int data_value = 1;
+    unsigned char temp_byte = 0x00;
+    int temp_binary_counting = 7;
+
+    while (1) {
+        while (clock_value == 0) {
+            clock_value = digitalRead(yingshaoxo_spi_input_clock_pin);
+            if ((millis()-start_time) > timeout) { yingshaoxo_spi_input_that_transmit_ends_with_04[index+1] = '\0'; return index; }
+        }
+        while (clock_value == 1) {
+            clock_value = digitalRead(yingshaoxo_spi_input_clock_pin);
+            if ((millis()-start_time) > timeout) { yingshaoxo_spi_input_that_transmit_ends_with_04[index+1] = '\0'; return index; }
+        }
+
+        data_value = digitalRead(yingshaoxo_spi_input_data_pin);
+        digitalWrite(13, !digitalRead(13)); //LED
+
+        // temo_binary_counting: 7,6,5,...,0
+        temp_byte |= (data_value << temp_binary_counting);
+        temp_binary_counting -= 1;
+        if (temp_binary_counting < 0) {
+            temp_binary_counting = 7;
+
+            if (temp_byte == 0x04) {
+                yingshaoxo_spi_input_that_transmit_ends_with_04[index] = '\0';
+                return index;
+            }
+
+            yingshaoxo_spi_input_that_transmit_ends_with_04[index] = temp_byte;
+            temp_byte = 0x00;
+            index += 1;
+
+            if (index >= 63) {
+                yingshaoxo_spi_input_that_transmit_ends_with_04[64] = '\0';
+                return 64;
+            }
+        }
+    }
+}
+
+// fake spi output: d2(clock), d3(data)
+#define yingshaoxo_spi_output_clock_pin 2
+#define yingshaoxo_spi_output_data_pin 3
+unsigned int yingshaoxo_spi_output_has_been_initialized = 0;
+void set_up_yingshaoxo_spi_output() {
+    pinMode(13, OUTPUT); // LED
+    pinMode(yingshaoxo_spi_output_clock_pin, OUTPUT);
+    pinMode(yingshaoxo_spi_output_data_pin, OUTPUT);
+    digitalWrite(yingshaoxo_spi_output_clock_pin, LOW);
+    digitalWrite(yingshaoxo_spi_output_data_pin, LOW);
+    yingshaoxo_spi_output_has_been_initialized = 1;
+}
+void send_yingshaoxo_spi_a_byte(unsigned char a_byte, int delay_in_millisecond) {
+    int temp_0_or_1 = 0;
+    unsigned char temp_binary_index = 0;
+    while (1) {
+        temp_0_or_1 = a_byte & (1 << (7-temp_binary_index));
+        digitalWrite(yingshaoxo_spi_output_data_pin, temp_0_or_1);
+        digitalWrite(yingshaoxo_spi_output_clock_pin, LOW);
+        delay(delay_in_millisecond);
+        digitalWrite(yingshaoxo_spi_output_clock_pin, HIGH);
+        delay(delay_in_millisecond);
+        digitalWrite(13, !digitalRead(13)); // LED
+        temp_binary_index += 1;
+        if (temp_binary_index >= 8) {
+            break;
+        }
+    }
+}
+void send_yingshaoxo_spi_output(unsigned char *data, int delay_in_millisecond) {
+    if (yingshaoxo_spi_output_has_been_initialized == 0) {
+        // call set_up_yingshaoxo_spi_output() first
+        return 0;
+    }
+    send_yingshaoxo_spi_a_byte(0x01, delay_in_millisecond);
+    send_yingshaoxo_spi_a_byte(0x02, delay_in_millisecond);
+    unsigned int index = 0;
+    unsigned char a_byte = 0x00;
+    while (1) {
+        a_byte = data[index];
+        send_yingshaoxo_spi_a_byte(a_byte, delay_in_millisecond);
+        index += 1;
+        if (a_byte == '\0') {
+            break;
+        }
+        if (a_byte == 0x04) {
+            break;
+        }
+        if (index >= 63) {
+            break;
+        }
+    }
+    send_yingshaoxo_spi_a_byte(0x04, delay_in_millisecond);
+}
+
+
+#endif
+
+/*
+void setup() {
+    set_up_yingshaoxo_spi_input();
+    set_up_yingshaoxo_spi_output();
+}
+
+void loop() {
+    //int result_index = get_yingshaoxo_spi_input(2000);
+    //if (result_index >= 1) {
+    //    clear_the_screen();
+    //    smart_print_string(0, 0, yingshaoxo_spi_input_that_transmit_ends_with_04);
+    //}
+
+    send_yingshaoxo_spi_output("hi, you!", 1);
+    delay(1000);
+
+    //int result_index = get_yingshaoxo_spi_input_bytes(0x01, 0x04);
+    //if (result_index >= 0) {
+    //    clear_the_screen();
+    //    smart_print_string(0, 0, yingshaoxo_spi_input_that_transmit_ends_with_04);
+    //}
+}
+*/ 
+
+
+
+/*
+class Simple_Output_Soft_SPI:
+    def __init__(self, sck, mosi):
+        self.sck = sck
+        self.sck.init(self.sck.OUT)
+        self.sck.low()
+        self.mosi = mosi
+        self.mosi.init(self.mosi.OUT)
+        self.mosi.low()
+
+    def _write_bit(self, bit):
+        # python do not need delay, because it is slow. delay after each clock signal
+        self.mosi.value(bit)
+        self.sck.high()
+        self.sck.low()
+
+    def write(self, data):
+        if isinstance(data, int):
+            data = [data]
+        for byte in data:
+            for bit_idx in range(7, -1, -1):
+                # write 0x01 as 00000001, the other side first receive 0, then 1.
+                bit = (byte >> bit_idx) & 0x01
+                self._write_bit(bit)
+
+
+class Simple_Input_Soft_SPI:
+    def __init__(self, sck, miso):
+        self.sck = sck
+        self.sck.init(self.sck.IN)
+        self.miso = miso
+        self.miso.init(self.miso.IN)
+
+    def read_0_or_1(self):
+        clock_value = 1
+        while clock_value == 1:
+            clock_value = self.sck.value()
+        while clock_value == 0:
+            clock_value = self.sck.value()
+        return self.miso.value()
+
+    def read_a_byte(self):
+        # will block/stuck program if no data. but quick.
+        # sender should at least delay for 1 millisecond.
+        a_byte = 0x00
+        i = 7;
+        get_value = 0
+        while 1:
+            get_value = self.read_0_or_1()
+            a_byte |= (get_value << i)
+            i -= 1
+            if i < 0:
+                return a_byte
+
+    def read_until_bytes(self, binary_0_and_1_list):
+        # 0x01: 00000001, 0x02: 00000010
+        length = len(binary_0_and_1_list)
+        temp_list = []
+        while 1:
+            temp_list.append(self.read_0_or_1())
+            if len(temp_list) == length:
+                if temp_list == binary_0_and_1_list:
+                    return
+                temp_list.pop(0)
+
+    def read_bytes(self, length=59, end_with=0x04):
+        # sender should at least delay for 1 millisecond
+        # add this before function will increase accuracy: my_input_spi.read_until_bytes([0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0]) #0x01,0x02
+        a_list = []
+        a_byte = 0x00
+        while len(a_list) < length:
+            a_byte = self.read_a_byte()
+            a_list.append(a_byte)
+            if a_byte == 0x04:
+                break
+        return bytes(a_list)
+
+
+"""
+from machine import Pin
+from soft_spi import Simple_Input_Soft_SPI, Simple_Output_Soft_SPI
+my_input_spi = Simple_Input_Soft_SPI(Pin(20), Pin(21))
+my_output_spi = Simple_Output_Soft_SPI(Pin(18), Pin(19))
+
+my_output_spi.write(bytes([0x01]) + b"what is your name?" + bytes([0x04]))
+
+# while True:
+#     my_input_spi.read_until_bytes([0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0])
+#     data = my_input_spi.read_bytes()
+#     print(data)
+"""
+*/
--- example/image/micropython/soft_spi.py
+++ example/image/micropython/soft_spi.py
@@ -1,25 +1,14 @@
-# this module written by doubao ai
-# fixed by yingshaoxo
-from time import sleep_us
-try:
-    from machine import Pin
-except Exception as e:
-    try:
-        from pyb import Pin
-    except Exception as e:
-        raise(e)
-
-
 class Simple_Output_Soft_SPI:
-    def __init__(self, sck, mosi, miso=None):
+    def __init__(self, sck, mosi):
         self.sck = sck
-        self.sck.init(Pin.OUT)
+        self.sck.init(self.sck.OUT)
         self.sck.low()
         self.mosi = mosi
-        self.mosi.init(Pin.OUT)
+        self.mosi.init(self.mosi.OUT)
         self.mosi.low()
 
     def _write_bit(self, bit):
+        # python do not need delay, because it is slow. delay after each clock signal
         self.mosi.value(bit)
         self.sck.high()
         self.sck.low()
@@ -29,102 +18,143 @@ class Simple_Output_Soft_SPI:
             data = [data]
         for byte in data:
             for bit_idx in range(7, -1, -1):
+                # write 0x01 as 00000001, the other side first receive 0, then 1.
                 bit = (byte >> bit_idx) & 0x01
                 self._write_bit(bit)
 
 
-class SoftSPI:
-    def __init__(self, sck, mosi, miso, delay=0):
-        if sck is None or mosi is None:
-            raise ValueError("SCK and MOSI pins must be specified for SoftSPI")
+class Simple_Input_Soft_SPI:
+    def __init__(self, sck, miso):
+        self.sck = sck
+        self.sck.init(self.sck.IN)
+        self.miso = miso
+        self.miso.init(self.miso.IN)
+
+    def read_0_or_1(self):
+        clock_value = 1
+        while clock_value == 1:
+            clock_value = self.sck.value()
+        while clock_value == 0:
+            clock_value = self.sck.value()
+        return self.miso.value()
+
+    def read_a_byte(self):
+        # will block/stuck program if no data. but quick.
+        # sender should at least delay for 1 millisecond.
+        a_byte = 0x00
+        i = 7;
+        get_value = 0
+        while 1:
+            get_value = self.read_0_or_1()
+            a_byte |= (get_value << i)
+            i -= 1
+            if i < 0:
+                return a_byte
+
+    def read_until_bytes(self, binary_0_and_1_list):
+        # 0x01: 00000001, 0x02: 00000010
+        length = len(binary_0_and_1_list)
+        temp_list = []
+        while 1:
+            temp_list.append(self.read_0_or_1())
+            if len(temp_list) == length:
+                if temp_list == binary_0_and_1_list:
+                    return
+                temp_list.pop(0)
+
+    def read_bytes(self, length=59, end_with=0x04):
+        # sender should at least delay for 1 millisecond
+        # add this before function will increase accuracy: my_input_spi.read_until_bytes([0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0]) #0x01,0x02
+        a_list = []
+        a_byte = 0x00
+        while len(a_list) < length:
+            a_byte = self.read_a_byte()
+            a_list.append(a_byte)
+            if a_byte == 0x04:
+                break
+        return bytes(a_list)
+
+
+class Complex_Input_Soft_SPI:
+    def __init__(self, sck, miso):
+        from time import time
+        self.time = time
 
         self.sck = sck
-        self.sck.init(Pin.OUT)
-        self.sck.low()
-
-        self.mosi = mosi
-        self.mosi.init(Pin.OUT)
-        self.mosi.low()
-
+        self.sck.init(self.sck.IN)
         self.miso = miso
-        if self.miso is not None:
-            self.miso.init(Pin.IN, Pin.PULL_NONE)
+        self.miso.init(self.miso.IN)
 
-        # Calculate delay for clock timing (1/(2*baudrate) = half clock cycle)
-        # Convert to microseconds for sleep_us (1e6 us = 1 second)
-        #self.clock_delay = int((1 / (2 * baudrate)) * 1e6) if baudrate > 0 else 0
-        self.clock_delay = delay
+    def _8_number_to_byte(self, a_list):
+        a_byte = 0x00
+        i = 0
+        while 1:
+            a_byte |= (a_list[i] << (7-i))
+            i += 1
+            if i >= 8:
+                break
+        return a_byte
 
-    def _clock_pulse(self, out_bit=None):
-        # Step 1: Set MOSI level (if writing)
-        if out_bit is not None:
-            self.mosi.value(out_bit)
+    def read_safely(self, timeout=5, start_byte=0x01, end_byte=0x04):
+        # the sender should at least delay for 10 milliseconds between clock switch to let the pyboard to receive data.
+        end_time = self.time() + timeout
+        temp_byte_array = bytearray()
+        receive_start = 0
+        clock_value = 1
+        the_0_or_1_queue = []
+        the_0_or_1_queue_index = 0
+        while 1:
+            while clock_value == 1:
+                clock_value = self.sck.value()
+                if self.time() > end_time:
+                    return temp_byte_array
+            while clock_value == 0:
+                clock_value = self.sck.value()
+                if self.time() > end_time:
+                    return temp_byte_array
 
-        # Step 2: Short delay to stabilize signal before clock edge
-        if self.clock_delay > 0:
-            sleep_us(self.clock_delay)
+            the_value = self.miso.value()
 
-        # Step 3: Raise SCK (rising edge - slave latches MOSI, master samples MISO)
-        self.sck.high()
-
-        # Step 4: Delay to ensure stable reading
-        if self.clock_delay > 0:
-            sleep_us(self.clock_delay)
-
-        # Step 5: Read MISO bit (if available)
-        in_bit = self.miso.value() if self.miso is not None else 0
-
-        # Step 6: Lower SCK (falling edge)
-        self.sck.low()
-
-        return in_bit
-
-    def write(self, data):
-        if isinstance(data, int):
-            data = [data]
-
-        for byte in data:
-            for bit_idx in range(7, -1, -1):  # 7 = MSB, 0 = LSB
-                out_bit = (byte >> bit_idx) & 0x01  # Extract single bit
-                self._clock_pulse(out_bit)
-
-    def read(self, n_bytes):
-        if self.miso is None:
-            raise RuntimeError("MISO pin not specified - cannot read data")
-
-        result = bytearray()
-        for _ in range(n_bytes):
-            byte = 0
-            for bit_idx in range(7, -1, -1):  # MSB first
-                in_bit = self._clock_pulse()  # No write, only read
-                byte |= (in_bit << bit_idx)   # Build byte from bits
-            result.append(byte)
-        return result
-
-    def write_readinto(self, write_buf, read_buf):
-        if len(write_buf) != len(read_buf):
-            raise ValueError("Write and read buffers must have same length")
-        if self.miso is None:
-            raise RuntimeError("MISO pin not specified - cannot read data")
-
-        for i in range(len(write_buf)):
-            write_byte = write_buf[i]
-            read_byte = 0
-            # Transmit/receive one byte (MSB first)
-            for bit_idx in range(7, -1, -1):
-                out_bit = (write_byte >> bit_idx) & 0x01
-                in_bit = self._clock_pulse(out_bit)
-                read_byte |= (in_bit << bit_idx)
-            read_buf[i] = read_byte
+            if receive_start == 0:
+                the_0_or_1_queue.append(the_value)
+                if len(the_0_or_1_queue) == 8:
+                    if (self._8_number_to_byte(the_0_or_1_queue) == start_byte):
+                        receive_start = 1
+                        the_0_or_1_queue_index = 0
+                    else:
+                        the_0_or_1_queue.pop(0)
+            else:
+                the_0_or_1_queue[the_0_or_1_queue_index] = the_value
+                the_0_or_1_queue_index += 1
+                if the_0_or_1_queue_index >= 8:
+                    a_byte = self._8_number_to_byte(the_0_or_1_queue)
+                    if (a_byte == end_byte):
+                        return temp_byte_array
+                    else:
+                        temp_byte_array.append(a_byte)
+                    the_0_or_1_queue_index = 0
 
 
 #if __name__ == "__main__":
 #    TFT_CLK_PIN = Pin('X1')
 #    TFT_MOSI_PIN = Pin('X2')
 #
-#    spiTFT = Simple_Output_Soft_SPI(baudrate=600000, sck=TFT_CLK_PIN, mosi=TFT_MOSI_PIN)
+#    spiTFT = Simple_Output_Soft_SPI(sck=TFT_CLK_PIN, mosi=TFT_MOSI_PIN)
 #
 #    spiTFT.write(0x55)
 #    spiTFT.write([0xAA, 0x01])
 
 
+"""
+from machine import Pin
+from soft_spi import Simple_Input_Soft_SPI, Simple_Output_Soft_SPI
+my_input_spi = Simple_Input_Soft_SPI(Pin(20), Pin(21))
+my_output_spi = Simple_Output_Soft_SPI(Pin(18), Pin(19))
+
+my_output_spi.write(bytes([0x01]) + b"what is your name?" + bytes([0x04]))
+
+# while True:
+#     my_input_spi.read_until_bytes([0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0])
+#     data = my_input_spi.read_bytes()
+#     print(data)
+"""
